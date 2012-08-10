10ea39b6ea0b702037432ba1c53fd4f58e60ec9a
John Trammell <john.trammell@outsell.com>
2012-08-10 15:25:43 -0500

    finished simple vim setup script

--- setup.sh
+++ setup.sh
@@ -2,8 +2,22 @@
 
 git clone git@github.com:trammell/dotvim.git
 
-cd dotvim
+# create symlink to ~/.vim/
+if [ -d "~/.vim/" ]; then
+    echo "~/.vim/ already exists--aborting"
+    exit 1
+else
+    ln -s `pwd`/dotvim ~/.vim
+fi
 
+# symlink to the actual vimrc file
+if [ -e "~/.vimrc" ]; then
+    echo "~/.vimrc already exists--aborting"
+    exit 1
+else
+    ln -s ~/.vim/vimrc.vim ~/.vimrc
+fi
 
-
+# check out any submodules
+cd ~/.vim/ && git submodule init && git submodule update
 
